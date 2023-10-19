8a890f3962445dc4deaf7a484483c7c403f0c0ee
mauricio pignoli vargas <62521025+maumaucb18@users.noreply.github.com>
2023-10-19 12:40:21 -0300

    public

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 function verificarPrimo() {
-  let numero = parseInt(document.getElementById("numero").value);
-  let resultadoElement = document.getElementById("resultado");
+    var inputNumero = document.getElementById("numero");
+    var resultadoElement = document.getElementById("resultado");
 
-  if (numero <= 1) {
-      resultadoElement.textContent = "Por favor, insira um número maior que 1.";
-  } else {
-      let primo = true;
-      for ( i = 2; i <= Math.sqrt(numero); i++) {
-          if (numero % i === 0) {
-              primo = false;
-              break;
-          }
-      }
+    // Verificar se o campo está vazio ou não é um número
+    if (inputNumero.value.trim() === "" || isNaN(inputNumero.value)) {
+        resultadoElement.textContent = "Por favor, insira um número válido.";
+        return; 
+    }
 
-      if (primo) {
-          resultadoElement.textContent = numero + " é um número primo!";
-      } else{
-          resultadoElement.textContent = numero + " não é um número primo.";
-      }
-      
-  }
+    var numero = parseInt(inputNumero.value);
+
+    if (numero <= 1) {
+        resultadoElement.textContent = "Por favor, insira um número maior que 1.";
+    } else {
+        var primo = true;
+        for (var i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i === 0) {
+                primo = false;
+                break;
+            }
+        }
+
+        if (primo) {
+            resultadoElement.textContent = numero + " é um número primo!";
+        } else {
+            resultadoElement.textContent = numero + " não é um número primo.";
+        }
+    }
 }
+
